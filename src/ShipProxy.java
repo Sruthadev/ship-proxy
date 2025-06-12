7eb7f9d830119d15fca76645b877afd9c74ea907
sruthadev <sruthadev@gmail.com>
2025-06-12 21:45:42 +0530

    Ship proxy added

--- src/ShipProxy.java
+++ src/ShipProxy.java
@@ -1,5 +1,5 @@
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -16,9 +16,76 @@ public class ShipProxy {
 
     private static BlockingQueue<Socket> requestQueue = new LinkedBlockingQueue<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.out.println("Ship proxy starting on port: " + SHIP_PROXY_PORT);
 
+        ServerSocket proxySocket = new ServerSocket(SHIP_PROXY_PORT);
+
+        offshoreSocket = new Socket(OFFSHORE_HOST, OFFSHORE_PROXY_PORT);
+        offshoreOut = offshoreSocket.getOutputStream();
+        offshoreIn = offshoreSocket.getInputStream();
+
+        new Thread(() -> {
+            while (true){
+                try {
+                    Socket browserSocket = requestQueue.take();
+                    handleRequest(browserSocket);
+                } catch (Exception e){
+                    System.out.println("Error in request handler: " + e.getMessage());
+                }
+            }
+        }).start();
+
+        while (true){
+            Socket browserSocket = proxySocket.accept();
+            requestQueue.add(browserSocket);
+        }
+    }
+
+    private static void handleRequest(Socket browserSocket){
+        try (
+                InputStream browserIn = browserSocket.getInputStream();
+                OutputStream browserOut = browserSocket.getOutputStream();
+                ){
+                    ByteArrayOutputStream requestBuffer = new ByteArrayOutputStream();
+                    byte[] buffer = new byte[4096];
+                    int read;
+                    browserSocket.setSoTimeout(1000);
+
+                    while ((read = browserIn.read(buffer)) != -1){
+                        requestBuffer.write(buffer, 0, read);
+                        if(read < buffer.length) break;
+                    }
+
+                    byte[] requestBytes = requestBuffer.toByteArray();
+
+                    sendFramed(offshoreOut, requestBytes);
+
+                    byte[] responseBytes = receivedFramed(offshoreIn);
+
+                    browserOut.write(responseBytes);
+                    browserOut.flush();
+
+                    browserSocket.close();
+
+        }catch (Exception e){
+            System.out.printf("Error in browser request handling: " + e.getMessage());
+        }
+    }
+
+    public static void sendFramed(OutputStream offshoreOut, byte[] requestBytes) throws IOException {
+        DataOutputStream dos = new DataOutputStream(offshoreOut);
+        dos.write(requestBytes.length);
+        dos.write(requestBytes);
+        dos.flush();
+    }
+
+    public static byte[] receivedFramed(InputStream offshoreIn) throws IOException {
+        DataInputStream dis = new DataInputStream(offshoreIn);
+        int length = dis.readInt();
+        byte[] data = new byte[length];
+        dis.readFully(data);
+        return data;
     }
 }
